277f310f7a0b2391b6d6d576de71b86c91db2f75
Etienne <etienne.delorieux@gmail.com>
2019-01-30 09:27:34 -0500

    Added Footer, Recipes and ShoppingList components, added routing and some styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { RecipesComponent } from './components/recipes/recipes.component';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RecipesComponent,
+    ShoppingListComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
